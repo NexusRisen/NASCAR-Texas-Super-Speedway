f8a0edb9ffa26008e543f4c301add7647b10dd86
Nexusrisen <bdkohn91@gmail.com>
2017-10-06 12:19:12 -0400

    1.0

--- nascar/__resource.lua
+++ nascar/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 this_is_a_map 'yes'
-data_file 'GTXD_PARENTING_DATA' 'gtxdbigcity.meta'
+data_file 'GTXD_PARENTING_DATA' 'texas.meta'
 
 data_file 'DLC_ITYP_REQUEST' 'stream/texas.ityp'
 file 'stream/texas_1.ydr'
