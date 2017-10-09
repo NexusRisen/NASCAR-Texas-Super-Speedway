635188ff821e5b0fa7776d931fe1ee1ba2b77368
Nexusrisen <bdkohn91@gmail.com>
2017-10-09 01:57:58 -0400

    1.6

--- texas/__resource.lua
+++ texas/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 this_is_a_map 'yes'
 data_file 'GTXD_PARENTING_DATA' 'texas.meta'
 
-data_file 'DLC_ITYP_REQUEST' 'stream/texas.ityp'
+data_file 'DLC_ITYP_REQUEST' 'stream/texas_props.ityp'
 file 'stream/texas_1.ydr'
 file 'stream/texas_2.ydr'
 file 'stream/texas_3.ydr'
